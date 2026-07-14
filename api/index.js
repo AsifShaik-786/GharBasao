99ea5c9b1cf27a7d525e40c421cb1a98de8401b9
Asif Shaik <shaikmahammadasif673@gmail.com>
2026-07-14 16:15:13 +0530

    Fix CORS for PATCH requests

--- api/index.js
+++ api/index.js
@@ -37,13 +37,38 @@ app.use(
       'http://localhost:5173',
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: [
+  'GET',
+  'POST',
+  'PUT',
+  'PATCH',
+  'DELETE',
+  'OPTIONS',
+],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
-app.options('*', cors());
-
+app.options(
+  '*',
+  cors({
+    origin: [
+      'https://ghar-basao.vercel.app',
+      'https://ghar-basao-git-main-asif-shaiks-projects-5b67bb0c.vercel.app',
+      'http://localhost:5173',
+    ],
+    credentials: true,
+    methods: [
+      'GET',
+      'POST',
+      'PUT',
+      'PATCH',
+      'DELETE',
+      'OPTIONS',
+    ],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 app.use(cookieParser());
 app.set('trust proxy', 1);
 
